2efa3f5a46426479548bf77d5b44c9655515ff59
Otha D Hernandez <otha.hernandez@gmail.com>
2019-05-02 14:27:10 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,157 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def get_first_name_of_season_winner(data, season)
-  
+  data.each do |season_num,season_data|
+      if season_num == season
+    season_data.each do |contestant_data|
+      contestant_data.each do |detail, data|
+        if data == "Winner"
+            contestant_data.each do |detail,data|
+          if detail == "name"
+              return  data.split.first
+            end
+          end
+        end
+      end
+    end
+  end
+end
+end
+
+def get_contestant_name(data, occupation)
+  data.each do |season_num,season_data|
+    season_data.each do |contestant_data|
+      contestant_data.each do |detail, data|
+          if detail == "occupation"
+        contestant_data.each do |detail,data|
+              if data == occupation
+            contestant_data.each do |detail,data|
+                if detail == "name"
+                  return data
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+def count_contestants_by_hometown(data, hometown)
+  counter = 0;
+  data.each do |season_num,season_data|
+    season_data.each do |contestant_data|
+      contestant_data.each do |detail, data|
+          if detail == "hometown"
+        contestant_data.each do |detail,data|
+              if data == hometown
+                counter += 1
+              end
+            end
+          end
+        end
+      end
+    end
+    return counter
+end
+
+def get_occupation(data, hometown)
+  data.each do |season_num,season_data|
+    season_data.each do |contestant_data|
+      contestant_data.each do |detail, data|
+          if detail == "hometown"
+        contestant_data.each do |detail,data|
+              if data == hometown
+        contestant_data.each do |detail,data|
+              if detail == "occupation"
+                return data
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
+end
+
+def get_average_age_for_season(data, season)
+  ages = 0
+  num_of_ages = 0
+    data[season].each do |contestant_data|
+      contestant_data.each do |detail,data|
+        if detail == "age"
+          num_of_ages += 1
+          ages += data.to_f
+        end
+      end
+    end
+  return (ages/num_of_ages).round
 end
 
 
@@ -7,178 +159,20 @@ end
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def get_first_name_of_season_winner(data, season)
-#   data.each do |season_num,season_data|
-#       if season_num == season
-#     season_data.each do |contestant_data|
-#       contestant_data.each do |detail, data|
-#         if data == "Winner"
-#             contestant_data.each do |detail,data|
-#           if detail == "name"
-#               return  data.split.first
-#             end
-#           end
-#         end
-#       end
-#     end
-#   end
-# end
-# end
-
-# def get_contestant_name(data, occupation)
-#   data.each do |season_num,season_data|
-#     season_data.each do |contestant_data|
-#       contestant_data.each do |detail, data|
-#           if detail == "occupation"
-#         contestant_data.each do |detail,data|
-#               if data == occupation
-#             contestant_data.each do |detail,data|
-#                 if detail == "name"
-#                   return data
-#                 end
-#               end
-#             end
-#           end
-#         end
-#       end
-#     end
-#   end
-# end
-
-# def count_contestants_by_hometown(data, hometown)
-#   counter = 0;
-#   data.each do |season_num,season_data|
-#     season_data.each do |contestant_data|
-#       contestant_data.each do |detail, data|
-#           if detail == "hometown"
-#         contestant_data.each do |detail,data|
-#               if data == hometown
-#                 counter += 1
-#               end
-#             end
-#           end
-#         end
-#       end
-#     end
-#     return counter
-# end
-
-# def get_occupation(data, hometown)
-#   data.each do |season_num,season_data|
-#     season_data.each do |contestant_data|
-#       contestant_data.each do |detail, data|
-#           if detail == "hometown"
-#         contestant_data.each do |detail,data|
-#               if data == hometown
-#         contestant_data.each do |detail,data|
-#               if detail == "occupation"
-#                 return data
-#               end
-#             end
-#           end
-#         end
-#       end
-#     end
-#   end
-# end
-# end
-
-# def get_average_age_for_season(data, season)
-#   ages = 0
-#   num_of_ages = 0
-#     data[season].each do |contestant_data|
-#       contestant_data.each do |detail,data|
-#         if detail == "age"
-#           num_of_ages += 1
-#           ages += data.to_f
-#         end
-#       end
-#     end
-#   return (ages/num_of_ages).round
-# end
-
-
-
-
-
-
-# def get_average_age_for_season(data, season)
-# ages = []
-#   data.each do |season_num,season_data|
-#       if season_num == season
-#     season_data.each do |contestant_data|
-#       contestant_data.each do |detail, data|
-#         if detail == "age"
-#               ages << data.to_i
-#             end
-#           end
-#         end
-#       end
-#     end
-# return ages.inject{|num_1, num_2| num_1 + num_2 }.to_i/ ages.size
-# end
+def get_average_age_for_season(data, season)
+ages = []
+  data.each do |season_num,season_data|
+      if season_num == season
+    season_data.each do |contestant_data|
+      contestant_data.each do |detail, data|
+        if detail == "age"
+              ages << data.to_i
+            end
+          end
+        end
+      end
+    end
+return ages.inject{|num_1, num_2| num_1 + num_2 }.to_i/ ages.size
+end
 
 
